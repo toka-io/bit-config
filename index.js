e38a9fbd6dca83e19db449ea97a1cd844da9af01
Andy Lim <andytlim@gmail.com>
2017-06-20 21:43:24 -0700

    Refactored config classes into separate classes for abstraction. Added unit tests.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = new (require('./lib/BitConfig.js'))();
+/**
+ * bit-config - A library that provides access to config file properties using dot notation
+ *
+ * Supported file types:
+ *   .json
+ */
+const dir = process.env.NODE_CONFIG_DIR || './config';
+const type = process.env.NODE_CONFIG_TYPE || 'json';
+
+module.exports = new (require('./lib/BitConfig.js'))(dir, type).getConfig();
--- tst/test.js
+++ tst/test.js
@@ -1,3 +1,31 @@
-const config = require('../index.js');
+const expect = require('chai').expect;
+let config = require('../index.js');
 
-console.log(config.get('prod.password'));
+/**
+ * Test BitConfig methods
+ */
+describe('BitConfig', () => {
+  it('get(property) - Get the property "prod.password" from config file', () => {
+    let value = config.get('prod.password');
+    expect(value).to.equal('kappa');
+  });
+  
+  it('get(property) - Throw error when trying to get a property that does not exist', () => {
+    expect(() => config.get('waifu')).to.throw('Property not found. Please make sure the property exists in your config file.');
+  });
+  
+  it('get(null) - Throw error when trying to get null property', () => {
+    expect(() => config.get(null)).to.throw('Property cannot be undefined or null');
+  });
+  
+  it('get() - Throw error when trying to get an undefined property', () => {
+    expect(() => config.get()).to.throw('Property cannot be undefined or null');
+  });
+  
+  it('require(module) - Throw error if config directory does not exist', () => {
+    // Reset cache to rebuild config with non-existent file directory
+    process.env['NODE_CONFIG_DIR'] = './build/config';
+    delete require.cache[require.resolve('../index.js')];
+    expect(() => require('../index.js')).to.throw('no such file');
+  });
+});
